5dcbf23095ba7579507d10ae0abc280fce7f9e09
AMOGUS1939 <petr1223334@gmail.com>
2024-08-01 23:50:16 +0300

    some unvaluable changes

--- src/main/java/paskhalko/test/testplugin/TestPlugin.java
+++ src/main/java/paskhalko/test/testplugin/TestPlugin.java
@@ -4,8 +4,11 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
+
+import java.nio.file.Path;
 
 @Plugin(
         id = "testplugin",
@@ -17,7 +20,7 @@ public class TestPlugin {
     private final Logger logger;
 
     @Inject
-    public TestPlugin(ProxyServer server, Logger logger) {
+    public TestPlugin(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
         this.server = server;
         this.logger = logger;
 
@@ -27,5 +30,7 @@ public class TestPlugin {
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
         System.out.println("TestPlugin init... ABOBA PASKHALKO PASKHALKO 1488");
+
+        //server.getEventManager().register(this, new PluginListener());
     }
 }
